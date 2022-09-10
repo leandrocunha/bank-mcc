eccb50a356ff3b95a77548485f36be90601bf95d
Leandro Cunha <leandroscunha@gmail.com>
2022-09-10 12:52:39 +0200

    feat: add ui skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { createUser } from './../api/User';
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    createUser({ name: 'Bianca Zanghi', email: 'biazanghi@gmail.com'})
-  }, []);
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <section className="main">
+        <Sidebar />
+        <section className="content">
+          {/* content here */}
+        </section>
+      </section>
     </div>
   )
 }

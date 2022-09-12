a529f267023f5077ef78cfc484e0f336f1133022
Leandro Cunha <LCunha3@its.jnj.com>
2022-09-12 22:29:41 +0200

    feat: add readFile method in the fileHandler api

--- api/utils/fileHandler/index.ts
+++ api/utils/fileHandler/index.ts
@@ -1,5 +1,12 @@
 import fs from 'vite-plugin-fs/browser';
 
+export interface IResponse {
+    statusCode: number
+    statusMessage: string
+    data: string | null
+    error: boolean
+}
+
 export const createFile = (content: string, filename: string, path: string): boolean => {
     try {
         fs.writeFile(`${path}${filename}`, content);
@@ -8,4 +15,24 @@ export const createFile = (content: string, filename: string, path: string): boo
     }
 
     return true
+}
+
+export const readFile = async (filePath: string): Promise<IResponse> => {
+    try {
+        const content = await fs.readFile(filePath);
+        return {
+            statusCode: 200,
+            statusMessage: 'Success',
+            data: content,
+            error: false
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            statusMessage: 'Error',
+            data: null,
+            error: true
+        }
+    }
+
 }

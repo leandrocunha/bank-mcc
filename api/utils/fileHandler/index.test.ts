c62bce773cfd9f5d2c2bdbc9987e4c21857d893e
Leandro Cunha <LCunha3@its.jnj.com>
2022-09-13 03:40:18 +0200

    fix: fileHandler unit tests

--- api/utils/fileHandler/index.test.ts
+++ api/utils/fileHandler/index.test.ts
@@ -2,13 +2,17 @@ import fs from 'vite-plugin-fs/browser';
 import { describe, expect, it, vi } from 'vitest';
 import { createFile } from './index';
 
-const spy = vi.spyOn(fs, 'writeFile');
+vi.mock('vite-plugin-fs/browser', () => ({
+    default: {
+        writeFile: vi.fn()
+    }
+}));
 
 describe('fileHandler', () => {
     it('should create a file', () => {
         const mockContent = 'My awesome content';
         const result = createFile(mockContent, 'myfilename.json', './data/');
-        expect(spy).toHaveBeenCalled()
+        expect(fs.writeFile).toHaveBeenCalled()
         expect(result).toBeTruthy();
     })
 })

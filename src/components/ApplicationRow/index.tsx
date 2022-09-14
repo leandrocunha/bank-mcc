57b5e29c6f9934de681ad84d815659eb583c6f43
Leandro Cunha <LCunha3@its.jnj.com>
2022-09-14 05:46:39 +0200

    feat: add Application details page

--- src/components/ApplicationRow/index.tsx
+++ src/components/ApplicationRow/index.tsx
@@ -4,6 +4,7 @@ import { IApplication } from '../../../api/Application'
 import { readFile } from '../../../api/utils/fileHandler'
 import { Heading } from '../Heading'
 import './index.css'
+import { Link } from 'react-router-dom'
 
 
 interface IProps {
@@ -27,7 +28,9 @@ export const ApplicationRow = (props: IProps): JSX.Element => {
     
     return (
         <div className="application-row">
-            <Heading className="application-row__name" size="h5" text={data?.name} />
+            <Link to={`/applications/${data?.uuid}`}>
+                <Heading className="application-row__name" size="h5" text={data?.name} />
+            </Link>
             <p className="application-row__uuid">{data?.uuid}</p>
             { data?.created_at && <TimeAgo className="application-row__created-at" date={data?.created_at} /> }
         </div>

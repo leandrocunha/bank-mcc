1dd20fd889cf887d54af7199bfddc5ad85948ca3
Leandro Cunha <LCunha3@its.jnj.com>
2022-09-14 11:19:43 +0200

    feat: enhancement in Application details page

--- src/pages/Applications/ApplicationsDetails/index.tsx
+++ src/pages/Applications/ApplicationsDetails/index.tsx
@@ -4,6 +4,7 @@ import { loadApplication } from "../../../../api/Application"
 import { Button } from "../../../components/Button"
 import { Heading } from "../../../components/Heading"
 import { Page } from "../../../components/Page"
+import './index.css'
 
 const PATH = './data/applications';
 
@@ -29,8 +30,45 @@ export const ApplicationsDetails = props => {
             <header className="main-page__header">
                 <Heading text={application?.name} />
                 <Button label="Back to Applications List" onClick={handleOnclick} />
-            </header>            
-            <p>App details</p>
+            </header>
+            <section className="main-page__section">
+                <div className="application-details">
+                    <header className="application-details__header">
+                        <div className="application-details__header__item">
+                            <span className="application-details__header__item__label">Name:</span>
+                            {application?.name}
+                        </div>
+                        <div className="application-details__header__item">
+                            <span className="application-details__header__item__label">UUID:</span>
+                            {application?.uuid}
+                        </div>
+                        <div className="application-details__header__item">
+                            <span className="application-details__header__item__label">Created at:</span>
+                            {application?.created_at}
+                        </div>
+                    </header>
+                    <main className="application-details__table">
+                       <header className="application-details__table__header">
+                            <p className="application-details__table__header__column-name">Configuration's versions</p>
+                            <p className="application-details__table__header__column-name">Created at</p>
+                       </header>
+                       <section className="application-details__rows">
+                            <div className="application-details__rows__row">
+                                <p>uuid</p>
+                                <p>12/24/1212</p>
+                            </div>
+                            <div className="application-details__rows__row">
+                                <p>uuid</p>
+                                <p>12/24/1212</p>
+                            </div>
+                            <div className="application-details__rows__row">
+                                <p>uuid</p>
+                                <p>12/24/1212</p>
+                            </div>                            
+                       </section>
+                    </main>
+                </div>
+            </section>
         </Page>
     )
 }

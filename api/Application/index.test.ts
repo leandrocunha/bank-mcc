0726e5ac8fc3dc6c78fae85986c91011517d6671
Leandro Cunha <LCunha3@its.jnj.com>
2022-09-15 06:57:25 +0200

    fix: missing dependencies for eslint and prettier

--- api/Application/index.test.ts
+++ api/Application/index.test.ts
@@ -1,34 +1,34 @@
-import { describe, expect, it, vitest } from 'vitest';
-import { createApplication, IApplication } from '.';
+import { describe, expect, it, vitest } from "vitest";
+import { createApplication, IApplication } from ".";
 
-describe('Application', () => {
-    let createFile: Function;
-    let mockData: IApplication;
+describe("Application", () => {
+  let createFile: Function;
+  let mockData: IApplication;
 
-    beforeEach(() => {
-        createFile = vitest.fn(() => 'mock');
-        mockData = {
-            uuid: '123-abc-456-def',
-            name: 'My awesome Application',
-            created_at: new Date()
-        }
-    });
+  beforeEach(() => {
+    createFile = vitest.fn(() => "mock");
+    mockData = {
+      uuid: "123-abc-456-def",
+      name: "My awesome Application",
+      created_at: new Date(),
+    };
+  });
 
-    it('should create a new application', () => {
-        const result = createApplication(mockData)(createFile);
-        expect(createFile).toBeCalled();
-        expect(result).toBeTypeOf('string');
-    })
+  it("should create a new application", () => {
+    const result = createApplication(mockData)(createFile);
+    expect(createFile).toBeCalled();
+    expect(result).toBeTypeOf("string");
+  });
 
-    it('should update a new application', () => {
-        const result = createApplication(mockData)(createFile);
-        expect(createFile).toBeCalled();
-        expect(result).toBeTypeOf('string');
-    })
+  it("should update a new application", () => {
+    const result = createApplication(mockData)(createFile);
+    expect(createFile).toBeCalled();
+    expect(result).toBeTypeOf("string");
+  });
 
-    it('should create a new application', () => {
-        const result = createApplication(mockData)(createFile);
-        expect(createFile).toBeCalled();
-        expect(result).toBeTypeOf('string');
-    })
-})
+  it("should create a new application", () => {
+    const result = createApplication(mockData)(createFile);
+    expect(createFile).toBeCalled();
+    expect(result).toBeTypeOf("string");
+  });
+});
--- api/Configuration/index.test.ts
+++ api/Configuration/index.test.ts
@@ -1,24 +1,24 @@
 import { describe, expect, it, vitest } from "vitest";
 import { createConfiguration, IConfiguration } from ".";
 
-describe('Configuration', () => {
-    let createFile: Function;
-    let mockData: IConfiguration;
+describe("Configuration", () => {
+  let createFile: Function;
+  let mockData: IConfiguration;
 
-    beforeEach(() => {
-        createFile = vitest.fn(() => 'mock');
-        mockData = {
-            uuid: '123-abc-456-def',
-            application: '987-xyz-654-klm',
-            author: '865-mdj-917-keh',
-            type: 'Metadata',
-            created_at: new Date()
-        }
-    });
+  beforeEach(() => {
+    createFile = vitest.fn(() => "mock");
+    mockData = {
+      uuid: "123-abc-456-def",
+      application: "987-xyz-654-klm",
+      author: "865-mdj-917-keh",
+      type: "Metadata",
+      created_at: new Date(),
+    };
+  });
 
-    it('should create a configuration', () => {
-        const result = createConfiguration(mockData)(createFile);
-        expect(createFile).toBeCalled();
-        expect(result).toBeTypeOf('object');
-    })
-})
+  it("should create a configuration", () => {
+    const result = createConfiguration(mockData)(createFile);
+    expect(createFile).toBeCalled();
+    expect(result).toBeTypeOf("object");
+  });
+});
